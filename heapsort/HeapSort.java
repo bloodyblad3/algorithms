54713fc7aa22fdcf84c5b39a375e74c7aedf8386
externalcharm <freesxsad23@mail.ru>
2023-06-16 10:31:23 +0300

    heapsort

--- heapsort/HeapSort.java
+++ heapsort/HeapSort.java
@@ -10,43 +10,36 @@ public class HeapSort {
     public static void sort(int[] arr) {
         int n = arr.length;
 
-        // Построение кучи (перегруппировка массива)
         for (int i = n / 2 - 1; i >= 0; i--)
             heapify(arr, n, i);
 
-        // Извлечение элементов из кучи
         for (int i = n - 1; i >= 0; i--) {
-            // Перемещаем текущий корень в конец
+
             int temp = arr[0];
             arr[0] = arr[i];
             arr[i] = temp;
 
-            // Вызываем процедуру heapify на уменьшенной куче
             heapify(arr, i, 0);
         }
     }
 
-    // Функция для построения кучи arr размером n с корнем в позиции i
     static void heapify(int[] arr, int n, int i) {
-        int largest = i; // Инициализируем наибольший элемент как корень
-        int left = 2 * i + 1; // Левый = 2*i + 1
-        int right = 2 * i + 2; // Правый = 2*i + 2
+        int largest = i; 
+        int left = 2 * i + 1; 
+        int right = 2 * i + 2;
 
-        // Если левый дочерний элемент больше корня
+
         if (left < n && arr[left] > arr[largest])
             largest = left;
 
-        // Если правый дочерний элемент больше, чем самый большой элемент на данный момент
         if (right < n && arr[right] > arr[largest])
             largest = right;
 
-        // Если самый большой элемент не корень
         if (largest != i) {
             int swap = arr[i];
             arr[i] = arr[largest];
             arr[largest] = swap;
 
-            // Продолжаем рекурсивно heapify поддерево
             heapify(arr, n, largest);
         }
     }
